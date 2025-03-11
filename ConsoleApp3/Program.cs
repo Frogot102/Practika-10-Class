38b5c2e933c520fa2c567465b3b4fe3f76a5eb8f
Frogot1 <alfiagilmutdinova30@gmail.com>
2025-03-11 14:49:25 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("N");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[N];
+            int sum = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine($"Введите {i + 1} слагаемое");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                sum = sum + array[i];
+
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("Ответ: " + sum);
+            Console.ReadKey();
+        }
+    }
+}
